4e581b2b722de05a44730a0c44b48927205eb0a9
Pradeep, Sakhamoori <pradeep.sakhamoori@intel.com>
2020-04-16 15:19:53 -0700

    OVDLstreamer class

--- gva-openvino-azure-iot/IntelGVAOpenVINO/modules/gva_ov_azure_iot/main.py
+++ gva-openvino-azure-iot/IntelGVAOpenVINO/modules/gva_ov_azure_iot/main.py
@@ -2,7 +2,6 @@
 # ==============================================================================
 import sys
 import json
-
 import gi
 #import iot_hub_manager
 
@@ -18,86 +17,93 @@ from gstgva import VideoFrame, util
 # Choose HTTP, AMQP or MQTT as transport protocol.  Currently only MQTT is supported.
 #IOT_HUB_PROTOCOL = IoTHubTransportProvider.MQTT
 #iot_hub_manager = IotHubManager(IOT_HUB_PROTOCOL)
-NUM_OF_INP_STREAMS = 1
 
-input="dlstreamer_test/car-detection.mp4"
-detection_model="dlstreamer_test/intel/vehicle-license-plate-detection-barrier-0106/FP32-INT8/vehicle-license-plate-detection-barrier-0106.xml"
-label_file="dlstreamer_test/intel/vehicle-license-plate-detection-barrier-0106/vehicle-license-plate-detection-barrier-0106.json"
+class OVDLStreamer():
 
-# File out options
-def create_launch_string(pipe):
-    return "filesrc location={} ! decodebin ! \
-    gvadetect model={} model_proc={} device=CPU batch-size=1 nireq=5 ! queue ! \
-    gvametaconvert ! gvametapublish ! gvafpscounter ! fakesink name=sink{} sync=false".format(input, detection_model, label_file, pipe)
+    def __init__(self, stream_type, stream_source, no_of_streams,  \
+                                            clas_model, det_model, label_file):
+        self.inp_stream_type ="file"
+        self.inp_stream_soruce="dlstreamer_test/car-detection.mp4"
+        self.num_of_inp_streams = 1
+        self.detection_model="dlstreamer_test/intel/vehicle-license-plate-detection-barrier-0106/FP32-INT8/vehicle-license-plate-detection-barrier-0106.xml"
+        self.label_file="dlstreamer_test/intel/vehicle-license-plate-detection-barrier-0106/vehicle-license-plate-detection-barrier-0106.json"
+
+    # File out options
+    def create_launch_string(self, pipe):
+        if(self.inp_stream_type == "file"):
+           return "filesrc location={} ! decodebin ! \
+               gvadetect model={} model_proc={} device=CPU batch-size=1 nireq=5 ! queue ! \
+               gvametaconvert ! gvametapublish ! gvafpscounter ! fakesink name=sink{} sync=false ".format(self.inp_stream_soruce, \
+                                                                                    self.detection_model, self.label_file, pipe)
+
+    def gobject_mainloop(self):
+       mainloop = GObject.MainLoop()
+       try:
+           mainloop.run()
+       except KeyboardInterrupt:
+           pass
+
+    def bus_call(self, bus, message, pipeline):
+        t = message.type
+        if t == Gst.MessageType.EOS:
+           print("pipeline ended")
+           pipeline.set_state(Gst.State.NULL)
+           sys.exit()
+        elif t == Gst.MessageType.ERROR:
+           err, debug = message.parse_error()
+           print("element {} error {} dbg {}".format(message.src.name, err, debug))
+        else:
+           pass
+        return True
+
+    def frame_callback(self, frame: VideoFrame):
+        for message in frame.messages():
+            m = json.loads(message)
+            #if iot_hub_manager is not None:
+            #iot_hub_manager.send_message_to_upstream(json.dumps(message))
+            print(m)
+
+    def pad_probe_callback(self, pad, info):
+        with util.GST_PAD_PROBE_INFO_BUFFER(info) as buffer:
+            caps = pad.get_current_caps()
+            frame = VideoFrame(buffer, caps=caps)
+            self.frame_callback(frame)
+
+        return Gst.PadProbeReturn.OK
 
 
-def gobject_mainloop():
-    mainloop = GObject.MainLoop()
-    try:
-        mainloop.run()
-    except KeyboardInterrupt:
-        pass
+    def set_callbacks(self, pipeline, pipe):
+        sink = pipeline.get_by_name("sink"+str(pipe))
+        pad = sink.get_static_pad("sink")
+        pad.add_probe(Gst.PadProbeType.BUFFER, self.pad_probe_callback)
 
-def bus_call(bus, message, pipeline):
-    t = message.type
-    if t == Gst.MessageType.EOS:
-        print("pipeline ended")
-        pipeline.set_state(Gst.State.NULL)
-        sys.exit()
-    elif t == Gst.MessageType.ERROR:
-        err, debug = message.parse_error()
-        print("element {} error {} dbg {}".format(message.src.name, err, debug))
-    else:
-        pass
-    return True
-
-
-def frame_callback(frame: VideoFrame):
-    for message in frame.messages():
-        m = json.loads(message)
-        #if iot_hub_manager is not None:
-        #    iot_hub_manager.send_message_to_upstream(json.dumps(message))
-        print(m)
-
-
-def pad_probe_callback(pad, info):
-    with util.GST_PAD_PROBE_INFO_BUFFER(info) as buffer:
-        caps = pad.get_current_caps()
-        frame = VideoFrame(buffer, caps=caps)
-        frame_callback(frame)
-
-    return Gst.PadProbeReturn.OK
-
-
-def set_callbacks(pipeline, pipe):
-    sink = pipeline.get_by_name("sink"+str(pipe))
-    pad = sink.get_static_pad("sink")
-    pad.add_probe(Gst.PadProbeType.BUFFER, pad_probe_callback)
-
-    bus = pipeline.get_bus()
-    bus.add_signal_watch()
-    bus.connect("message", bus_call, pipeline)
+        bus = pipeline.get_bus()
+        bus.add_signal_watch()
+        bus.connect("message", self.bus_call, pipeline)
 
 if __name__ == '__main__':
+
+    dlstr = OVDLStreamer("file", None, 1, None, None, None)
+
     Gst.init(sys.argv)
     gst_launch_string = ""
-    pipe = NUM_OF_INP_STREAMS
+    pipe = dlstr.num_of_inp_streams
 
     while(pipe):
-        gst_launch_string += create_launch_string(pipe)
+        gst_launch_string += dlstr.create_launch_string(pipe)
         pipe -= 1
 
     print(gst_launch_string)
     pipeline = Gst.parse_launch(gst_launch_string)
 
-    pipe = NUM_OF_INP_STREAMS
+    pipe = dlstr.num_of_inp_streams
 
     while(pipe):
-       set_callbacks(pipeline, pipe)
+       dlstr.set_callbacks(pipeline, pipe)
        pipe -= 1
 
     pipeline.set_state(Gst.State.PLAYING)
 
-    gobject_mainloop()
+    dlstr.gobject_mainloop()
 
     print("Exiting")
