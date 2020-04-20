6869f1049db1dac27a49dd0307c876954324a9ec
Pradeep, Sakhamoori <pradeep.sakhamoori@intel.com>
2020-04-20 13:45:00 -0700

    Vehicle detection, license plate detection and recogntion (chinese)

--- gva-openvino-azure-iot/IntelGVAOpenVINO/modules/gva_ov_azure_iot/main.py
+++ gva-openvino-azure-iot/IntelGVAOpenVINO/modules/gva_ov_azure_iot/main.py
@@ -23,18 +23,33 @@ class OVDLStreamer():
     def __init__(self, stream_type, stream_source, no_of_streams,  \
                                             clas_model, det_model, label_file):
         self.inp_stream_type ="file"
-        self.inp_stream_soruce="dlstreamer_test/car-detection.mp4"
+        self.inp_stream_source="dlstreamer_test/ch-lp.mp4"
         self.num_of_inp_streams = 1
-        self.detection_model="dlstreamer_test/intel/vehicle-license-plate-detection-barrier-0106/FP32-INT8/vehicle-license-plate-detection-barrier-0106.xml"
-        self.label_file="dlstreamer_test/intel/vehicle-license-plate-detection-barrier-0106/vehicle-license-plate-detection-barrier-0106.json"
+        self.veh_lp_det="dlstreamer_test/intel/vehicle-license-plate-detection-barrier-0106/FP32-INT8/vehicle-license-plate-detection-barrier-0106.xml"
+        #self.veh_att_model="dlstreamer_test/intel/vehicle-attributes-recognition-barrier-0039/FP32-INT8/vehicle-attributes-recognition-barrier-0039.xml"
+
+        #self.label_file="dlstreamer_test/intel/vehicle-license-plate-detection-barrier-0106/vehicle-license-plate-detection-barrier-0106.json"
+        self.lp_recog="dlstreamer_test/intel/license-plate-recognition-barrier-0001/FP32-INT8/license-plate-recognition-barrier-0001.xml"
+        self.label_file="dlstreamer_test/intel/license-plate-recognition-barrier-0001/license-plate-recognition-barrier-0001.json"
+        self.render = False
 
     # File out options
     def create_launch_string(self, pipe):
         if(self.inp_stream_type == "file"):
-           return "filesrc location={} ! decodebin ! \
-               gvadetect model={} model_proc={} device=CPU batch-size=1 nireq=5 ! queue ! \
-               gvametaconvert ! gvametapublish ! gvafpscounter ! fakesink name=sink{} sync=false ".format(self.inp_stream_soruce, \
-                                                                                    self.detection_model, self.label_file, pipe)
+            if(self.render):
+               return "filesrc location={} ! decodebin ! \
+                   gvadetect model={} device=CPU batch-size=1 nireq=5 ! queue ! \
+                   gvaclassify model={} model_proc={} device=CPU batch-size=1 nireq=5 ! queue ! \
+                   gvametaconvert ! gvametapublish ! videoconvert ! gvawatermark ! \
+                   fpsdisplaysink video-sink=xvimagesink name=sink{} async-handling=true".format(self.inp_stream_source, self.veh_lp_det,
+                                                                    self.lp_recog, self.label_file, pipe)
+            else:
+                return "filesrc location={} ! decodebin ! \
+                   gvadetect model={} device=CPU batch-size=1 nireq=5 ! queue ! \
+                   gvaclassify model={} model_proc={} device=CPU batch-size=1 nireq=5 ! queue ! \
+                   gvametaconvert ! gvametapublish ! gvafpscounter ! \
+                   fakesink name=sink{} sync=false ".format(self.inp_stream_source, self.veh_lp_det,
+                                                                    self.lp_recog, self.label_file, pipe)
 
     def gobject_mainloop(self):
        mainloop = GObject.MainLoop()
@@ -74,8 +89,10 @@ class OVDLStreamer():
 
     def set_callbacks(self, pipeline, pipe):
         sink = pipeline.get_by_name("sink"+str(pipe))
-        pad = sink.get_static_pad("sink")
-        pad.add_probe(Gst.PadProbeType.BUFFER, self.pad_probe_callback)
+
+        if(self.render == False):
+           pad = sink.get_static_pad("sink")
+           pad.add_probe(Gst.PadProbeType.BUFFER, self.pad_probe_callback)
 
         bus = pipeline.get_bus()
         bus.add_signal_watch()
